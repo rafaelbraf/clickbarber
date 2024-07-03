48470793883b8da701eac051e9d9afd26addc3b5
rafaelbraf <rafabrunofelix@hotmail.com>
2024-07-03 01:49:17 -0300

    [feat/frontend] CB-38 - Implementa página de serviços

---
+++ frontend-admin/src/models/Servico.ts
@@ -0,0 +1,7 @@
+export interface Servico {
+    idExterno: string;
+    nome: string;
+    preco: number;
+    tempoDuracaoEmMinutos: number;
+    ativo: boolean;
+}
---
+++ frontend-admin/src/services/ServicoService.ts
@@ -0,0 +1,25 @@
+import axios, { AxiosError } from "axios";
+import { apiUrl } from "../api/Constants";
+import { Servico } from "../models/Servico";
+
+export class ServicoService {
+    static async buscarServicosDaBarbearia(idExterno: string, token: string): Promise<Servico[]> {
+        try {
+            const response = await axios.get(`${apiUrl}/servicos/barbearia/${idExterno}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+
+            const data = await response.data;
+            return data.result;
+        } catch (error) {
+            const axiosError = error as AxiosError;
+            if (axiosError.response) {
+                throw axiosError.response;
+            }
+
+            throw error;
+        }
+    }
+}
