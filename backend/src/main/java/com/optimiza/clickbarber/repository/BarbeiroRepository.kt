ac437e223992f0112f2db43e5cc84b911742831e
rafaelbraf <rafabrunofelix@hotmail.com>
2024-07-09 08:49:30 -0300

    [fix/backend] Melhora query ao consultar Agendamentos

--- backend/src/main/java/com/optimiza/clickbarber/repository/BarbeiroRepository.kt
+++ backend/src/main/java/com/optimiza/clickbarber/repository/BarbeiroRepository.kt
@@ -7,11 +7,11 @@ import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
-interface BarbeiroRepository : JpaRepository<Barbeiro?, Long?> {
+interface BarbeiroRepository : JpaRepository<Barbeiro, Long> {
     @Query("SELECT b1 FROM Barbeiro b1 INNER JOIN Barbearia b2 ON b1.barbearia.id = b2.id WHERE b2.idExterno = :idExternoBarbearia")
-    fun findByIdExternoBarbearia(idExternoBarbearia: UUID?): List<Barbeiro?>?
+    fun findByIdExternoBarbearia(idExternoBarbearia: UUID?): List<Barbeiro>
 
-    fun findByBarbeariaId(id: Int?): List<Barbeiro?>?
+    fun findByBarbeariaId(id: Int): List<Barbeiro>
 
-    fun findByUsuarioId(usuarioId: Long?): Optional<Barbeiro?>?
+    fun findByUsuarioId(usuarioId: Long): Optional<Barbeiro>
 }
