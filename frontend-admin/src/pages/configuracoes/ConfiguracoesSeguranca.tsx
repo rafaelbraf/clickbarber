294c85ef54b3aac36787fd3bcc9e94ce3330527c
Rafael Bruno <rafabrunofelix@hotmail.com>
2024-07-26 23:57:37 -0300

    [feat/frontend-admin] Implementa página de configurações de segurança

--- frontend-admin/src/pages/configuracoes/ConfiguracoesSeguranca.tsx
+++ frontend-admin/src/pages/configuracoes/ConfiguracoesSeguranca.tsx
@@ -1,7 +1,49 @@
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
+
 export const ConfiguracoesSeguranca = () => {
     return (
         <div>
             <h2>Segurança</h2>
+            <Container className="mt-5">
+                <Row className="align-items-center">
+                    <Col><h3>Alterar senha</h3></Col>
+                    <Form.Group className="mt-3" as={Row} controlId="formSenhaAtual">
+                        <Form.Label column sm={2}>Senha atual</Form.Label>
+                        <Col sm={10}>
+                            <Form.Control
+                                type="text"
+                                placeholder="Senha atual"
+                            />
+                        </Col>
+                    </Form.Group>
+
+                    <Form.Group className="mt-3" as={Row} controlId="formNovaSenha">
+                        <Form.Label column sm={2}>Nova senha</Form.Label>
+                        <Col sm={10}>
+                            <Form.Control
+                                type="text"
+                                placeholder="Nova senha"
+                            />
+                        </Col>
+                    </Form.Group>
+
+                    <Form.Group className="mt-3" as={Row} controlId="formConfirmarNovaSenha">
+                        <Form.Label column sm={2}>Confirmar nova senha</Form.Label>
+                        <Col sm={10}>
+                            <Form.Control
+                                type="password"
+                                placeholder="Confirmar nova senha"
+                            />
+                        </Col>
+                    </Form.Group>
+
+                    <Form.Group className="mt-3" as={Row}>
+                        <Col sm={{ span: 10, offset: 2 }} className="text-end">
+                            <Button variant="primary" type="submit">Alterar senha</Button>
+                        </Col>
+                    </Form.Group>
+                </Row>
+            </Container>
         </div>
     );
 }
