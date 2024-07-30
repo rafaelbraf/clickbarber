4c209355cb738c4688c6129f9fd3e7fdf50fce75
Rafael Bruno <rafabrunofelix@hotmail.com>
2024-07-29 23:33:32 -0300

    [fix/frontend-admin] Corrige definição da variável de ambiente

--- frontend-admin/src/api/Constants.ts
+++ frontend-admin/src/api/Constants.ts
@@ -1,2 +1,2 @@
-export const apiUrl: string = import.meta.env.URL_BACKEND || 'http://localhost:8081';
+export const apiUrl: string = import.meta.env.VITE_URL_BACKEND || 'http://localhost:8081';
 export const token: string = "";
